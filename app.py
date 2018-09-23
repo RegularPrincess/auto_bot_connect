4aab9668b676e23834423506880099b1e6a52537
JMFedorov <jmfedorov@gmail.com>
2018-09-23 21:17:40 +0300

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import requests
 from flask import Flask
 # from flask import json
 from flask import request
@@ -23,15 +23,27 @@ from flask import request
 
 app = Flask(__name__)
 
+
+@app.route(rule='/auto_bot_connect', methods=['GET'])
+def debug():
+    code = request.args.get('code')
+    print(code)
+
+    url = 'https://oauth.vk.com/access_token?' \
+          'client_id=6700721&' \
+          'client_secret=7Qj2REiRNqQW71oJ6xy0&' \
+          'redirect_uri=http://89.223.88.106/auto_bot_connect&' \
+          'code={}'.format(code)
+    response = requests.post(url)
+    print(response)
+    print(response.text)
+    # dict_data = json.loads(response.text)
+    return "ok"
+
 if __name__ == '__main__':
     app.run()
 
 
-@app.route(rule='/', methods=['GET'])
-def debug():
-    code = request.args.get('code')
-    print(code)
-    return "ok"
 
 
 # @app.route(rule='/{0}'.format(bot_name), methods=['POST'])
